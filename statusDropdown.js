b1b65d51d28e85e4fea846243a89c2b128b41de8
thezigzag <pjbuonomo@me.com>
2023-06-23 14:00:49 -0400

    Update statusDropdown.js

--- statusDropdown.js
+++ statusDropdown.js
@@ -45,3 +45,56 @@ function retrieveStatusOptions() {
 
 // Call the function to retrieve status options and initialize the dropdown
 retrieveStatusOptions();
+function updateListItemsToFalse() {
+    // Load the SharePoint client object model
+    SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function() {
+      // Replace 'YourSiteUrl' with the actual URL of your SharePoint site
+      var siteUrl = 'YourSiteUrl';
+      
+      // Replace 'PurchaseRequests' with the actual name of your list
+      var listName = 'PurchaseRequests';
+      
+      // Create a new client context
+      var clientContext = new SP.ClientContext(siteUrl);
+      
+      // Get the list by its name
+      var list = clientContext.get_web().get_lists().getByTitle(listName);
+      
+      // Create a query to retrieve all items in the list
+      var query = SP.CamlQuery.createAllItemsQuery();
+      var items = list.getItems(query);
+      
+      // Load the 'isSendEmailToRequestor' and 'isSendEmailToAgent' fields for all items
+      clientContext.load(items, 'Include(isSendEmailToRequestor,isSendEmailToAgent)');
+      
+      // Execute the query to retrieve the items
+      clientContext.executeQueryAsync(
+        function() {
+          // Iterate through the items and update the fields to false
+          var enumerator = items.getEnumerator();
+          while (enumerator.moveNext()) {
+            var item = enumerator.get_current();
+            item.set_item('isSendEmailToRequestor', false);
+            item.set_item('isSendEmailToAgent', false);
+            item.update();
+          }
+          
+          // Execute the update operation
+          clientContext.executeQueryAsync(
+            function() {
+              console.log('Values updated successfully.');
+            },
+            function(sender, args) {
+              console.log('Error updating values: ' + args.get_message());
+            }
+          );
+        },
+        function(sender, args) {
+          console.log('Error retrieving items: ' + args.get_message());
+        }
+      );
+    });
+  }
+  
+  // Call the function to update the list items
+  updateListItemsToFalse();
