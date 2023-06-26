3de8a1f97bbd6b37b7386aa04077a00d93554a84
thezigzag <pjbuonomo@me.com>
2023-06-26 12:39:46 -0400

    Update activityContentTypes.js

--- activityContentTypes.js
+++ activityContentTypes.js
@@ -51,13 +51,75 @@ function constructCommentSection(itemId) {
     }
 
     /* Function to add a new activity (previously addComment) */
-    function addActivity(type, message) {
-        let timestamp = new Date().toLocaleString();
-        let username = currentUser.Title;
-        let newCard = createCard(type, username, message, timestamp);
-        $("#ActivityLog").append(newCard);
-        // Rest of the logic to update SharePoint item with new activity
+    /* Function to add a new activity */
+function addActivity(type, content) {
+    let timestamp = new Date().toLocaleString();
+    let username = currentUser.Title;
+
+    let newCard = createCard(type, username, content, timestamp);
+    $("#ActivityLog").append(newCard);
+
+    // Retrieve the existing activity history
+    let activityHistory = null;
+    let authorId = null;
+
+    $.ajax({
+        url: siteUrl + "/_api/web/lists/GetByTitle('" + listName + "')/items(" + currentItemID + ")",
+        method: "GET",
+        headers: {
+            "Accept": "application/json; odata=verbose"
+        },
+        async: false,
+        success: function(data) {
+            activityHistory = data.d.HistoryLog;
+            authorId = data.d.AuthorId;
+        },
+        error: function(error) {
+            console.error('Request failed. ' + JSON.stringify(error));
+        }
+    });
+
+    // Update the activity history with the new activity
+    activityHistory = activityHistory ? activityHistory + '\n' + username + ' {' + type + '} (' + timestamp + '): ' + content : username + ' {' + type + '} (' + timestamp + '): ' + content;
+
+    let isSendMessageToRequestor = false;
+    let isSendMessageToAgent = false;
+    if(currentUser.Id === authorId) {
+        isSendMessageToAgent = true;
+    } else {
+        isSendMessageToRequestor = true;
     }
+
+    // Update the item with the modified activity history
+    $.ajax({
+        url: siteUrl + "/_api/web/lists/GetByTitle('" + listName + "')/items(" + currentItemID + ")",
+        type: "POST",
+        data: JSON.stringify({
+            '__metadata': {
+                'type': 'SP.Data.' + listName + 'ListItem'
+            },
+            'HistoryLog': activityHistory,
+            'NewComment1': content,
+            'CommentingUser1Id': currentUser.Id,
+            'IsSendMessageToRequestor': isSendMessageToRequestor,
+            'IsSendMessageToAgent': isSendMessageToAgent
+        }),
+        headers: {
+            "X-RequestDigest": $("#__REQUESTDIGEST").val(),
+            "accept": "application/json;odata=verbose",
+            "IF-MATCH": "*",
+            "content-type": "application/json;odata=verbose",
+            "X-HTTP-Method": "MERGE"
+        },
+        success: function() {
+            console.log('Activity has been added successfully.');
+        },
+        error: function(error) {
+            console.error('Request failed. ' + JSON.stringify(error));
+        }
+    });
+}
+
 
     /* Function to get query string parameter */
     function getQueryStringParameter(name) {
